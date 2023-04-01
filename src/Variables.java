221ee15e55bc5de3a1eb57ff51b0d66fb774fcf2
kimovl <mokhov_kyrylo@gmail.com>
2023-04-01 11:32:42 +0300

    Basic primitives data types

--- src/Variables.java
+++ src/Variables.java
@@ -1,3 +1,19 @@
 public class Variables {
+    public static void main(String[] args) {
+        int myInt;
+        myInt = 55;
+        short myShort = 555;
+        long myLong = 55555555;
 
+        double myDouble = 2.2;//64 bit
+        float myFloat = 2.2f;//32 bit
+
+        char myChar = 'a';
+        boolean myBoolean = true;
+
+        byte myByte = 33;//-128~127
+        System.out.println("int - " + myInt + ", short - " + myShort + ", long - " + myLong);
+        System.out.println("double - " + myDouble + ", float - " + myFloat);
+        System.out.println("byte - " + myByte);
+    }
 }
