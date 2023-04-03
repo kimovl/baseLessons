a014facb3d74ea1cef8d1bf1a8a11352c7a65c0f
kimovl <mokhov_kyrylo@gmail.com>
2023-04-03 20:17:10 +0300

    Basic usage IF construction

--- src/If.java
+++ src/If.java
@@ -1,5 +1,20 @@
 public class If {
     public static void main(String[] args) {
-
+        int first_value = 10;
+        if (6 == 6) {
+            System.out.println("6 = 6 ? - " + "It`s correct");
+        }
+        if (5 < 4) {
+            System.out.println("It`s correct");
+        } else {
+            System.out.println("It`s incorrect");
+        }
+        if (first_value >= 11) {
+            System.out.println("CORRECT");
+        } else if (first_value >= 20) {
+            System.out.println("CORRECT >= 20");
+        } else {
+            System.out.println("DEFAULT");
+        }
     }
 }
