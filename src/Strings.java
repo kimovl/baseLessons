c4e11d2da22bc63db0730172042daad150518cb3
kimovl <mokhov_kyrylo@gmail.com>
2023-04-03 20:03:14 +0300

    Basic usage String

---
+++ src/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        String str = "Hello";//It`s class object not primitive data type
+        String name = "Bob";
+        String space = " ";
+        System.out.println(str + space + name);//concat operation
+    }
+}
