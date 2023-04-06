e4d6f89264b18ca5c766752f99b1dd57d7044a1c
kimovl <mokhov_kyrylo@gmail.com>
2023-04-06 21:10:09 +0300

    Basic usage Scanner from java.util.Scanner

--- src/Input.java
+++ src/Input.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input something: ");
+        String str = scanner.nextLine();//line for something from console and have methods for int double etc.
+        System.out.println("Your input is: " + str);
+    }
 }
