256fb8f9290b3eda54bcb6fcd918a647eef3e334
kimovl <mokhov_kyrylo@gmail.com>
2023-04-06 21:18:45 +0300

    Basic usage while, do while loops

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class DoWhile {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter some integer value");
+        int value = scanner.nextInt();
+        /*while (value != 5) {
+            System.out.println("Please, input 5 to stop");
+            value = scanner.nextInt();
+        }
+        System.out.println("Congrat, you enter 5");*/
+        do {
+            System.out.println("Please, input 5 to stop");
+            value = scanner.nextInt();
+        } while (value != 5);
+        System.out.println("Congrat, you enter 5");
     }
 }
