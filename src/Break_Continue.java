4bbcc48fc137832c5d7ab0eba8e5c753bfd002df
kimovl <mokhov_kyrylo@gmail.com>
2023-05-22 20:49:03 +0300

    Added new class Break_Continue

--- src/Break_Continue.java
+++ src/Break_Continue.java
@@ -1,2 +1,10 @@
 public class Break_Continue {
+    public static void main(String[] args) {
+        int i = 0;
+        while (true) {
+            System.out.println(i);
+            i++;
+            if (i == 6) break;
+        }
+    }
 }
