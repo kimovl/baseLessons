51bfdaf58a04051f1b2db79413cd57d42ed18704
kimovl <mokhov_kyrylo@gmail.com>
2023-05-22 20:52:22 +0300

    break and continue simple usage

--- src/Break_Continue.java
+++ src/Break_Continue.java
@@ -1,10 +1,16 @@
 public class Break_Continue {
     public static void main(String[] args) {
-        int i = 0;
+        /*int i = 0;
         while (true) {
             System.out.println(i);
             i++;
             if (i == 6) break;
+        }*/
+        for (int j = 0; j <= 15; j++) {
+            if (j % 2 == 0) {
+                continue;
+            }
+            System.out.println("N%2 number - " + j);
         }
     }
 }
